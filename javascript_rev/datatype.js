0ed100edcc6a8230053083b57f65c89a5cb4899d
Swarup143 <santiswarupnayak1@gmail.com>
2026-02-21 18:01:10 +0530

    datatype

---
+++ javascript_rev/datatype.js
@@ -0,0 +1,24 @@
+"use strict"//treat all js as newer version
+
+//alert("hello")
+//alert(3+3)
+//we are using node not browser
+console.log(3 + 3)
+let name = "hitesh"
+let age = 18
+let isloggedIn = false
+
+//number => 2 power 53
+//bigint
+//string=""
+//boolean=true / false
+//null : a stand alone value/represntation of empty value
+//undefined :
+//symbol : uniqueness
+
+//object
+console.log(typeof "san")
+console.log(typeof age)
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
+
