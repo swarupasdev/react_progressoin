cc81b0b4f0b96d889a74cf551c8564657a44a484
Swarup143 <santiswarupnayak1@gmail.com>
2026-02-21 20:14:27 +0530

    conversion

---
+++ javascript_rev/conversion.js
@@ -0,0 +1,22 @@
+let score = null
+console.log(typeof(score))
+
+let scoreInt = Number(score)
+
+console.log(typeof (scoreInt))
+console.log(scoreInt)
+//"33" => 33
+//"33jsdj"=> NaN
+//true=1 false = 0
+
+let isLoggedIn = 1
+let boolisLoggedin = Boolean(isLoggedIn)
+console.log(boolisLoggedin)
+
+//1= true 0=false
+//empty strings ""= flase
+//"anything"=true
+
+let any = 33
+let anytostring = String(any)
+console.log(anytostring)
