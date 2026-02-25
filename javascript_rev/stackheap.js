b8b5b7c82a5c41ad275119fa80f415b20705e3bf
Swarup143 <santiswarupnayak1@gmail.com>
2026-02-25 20:35:24 +0530

    stack heap

--- javascript_rev/stackheap.js
+++ javascript_rev/stackheap.js
@@ -10,13 +10,18 @@ let user1 = {
     email: "santi@gmail.com",
     upi: "user@ybl"
 }
+
+let user2=user1
 console.log(user1)
+
+
+//     user2
 //   ---------------        _________________________
-//                          |                       |
+//     user1                |                       |
 //   ---------------        |                       |
-//                          |                       |
-//   ---------------        |                       |
-//                          |                       |
+//     anything             |                       |
+//   ---------------        |email:"santi@gmail.com"|
+//     something            | ,upi: "user@ybl"      |
 //   ---------------        |                       |
 //     anything             |                       |
 //   ---------------        -------------------------
