976fdc8bd70ae07ca96ec7432fa2c9a9ad82e4ef
Swarup143 <santiswarupnayak1@gmail.com>
2026-02-25 22:39:56 +0530

    string

---
+++ javascript_rev/string.js
@@ -0,0 +1,11 @@
+const name = "san"
+const repocount = "20"
+console.log(`hello my name is  ${name} and my repo count is ${repocount}`)
+
+const gamename = new String('Cricket')
+//new keyword is for creating a object
+
+console.log(gamename)
+console.log(gamename.__proto__)
+
+console.log(gamename.length)
