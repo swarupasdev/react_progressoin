d9d710c1916043ff21105e6ef64f263a1361ea19
Swarup143 <santiswarupnayak1@gmail.com>
2026-03-02 23:17:48 +0530

    date and time

---
+++ javascript_rev/dateandtime.js
@@ -0,0 +1,13 @@
+//dates
+
+let myDate = new Date()
+console.log(myDate)
+console.log(typeof(myDate))
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleString())
+
+
+let mynewDate = new Date(2002, 9, 11)
+console.log(mynewDate.toString())
+console.log(mynewDate)
