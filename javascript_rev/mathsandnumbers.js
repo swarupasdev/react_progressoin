d96da8ec30a0893b9ec4c8c2ddc22424763400d5
Swarup143 <santiswarupnayak1@gmail.com>
2026-03-02 21:52:42 +0530

    maths and numbers

--- javascript_rev/mathsandnumbers.js
+++ javascript_rev/mathsandnumbers.js
@@ -12,3 +12,18 @@ const newBalance = new Number(29)
 console.log(newBalance.toPrecision(5)) //scientific notation
 console.log(typeof(newBalance))
 console.log(newBalance)
+
+
+//-----------Maths------------
+console.log(Math)
+console.log(Math.abs(-6))
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.6))
+console.log(Math.floor(4.6))
+console.log(Math.min(4, 3, 6, 8))
+console.log(Math.max(4, 3, 6, 8))
+
+console.log(Math.random())
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min+1)))
