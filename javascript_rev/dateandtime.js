896be340e07dc04d2a16221bffd8edbabd525293
Swarup143 <santiswarupnayak1@gmail.com>
2026-03-03 23:55:09 +0530

    date and time

--- javascript_rev/dateandtime.js
+++ javascript_rev/dateandtime.js
@@ -1,13 +1,29 @@
 //dates
 
-let myDate = new Date()
-console.log(myDate)
-console.log(typeof(myDate))
-console.log(myDate.toString())
-console.log(myDate.toDateString())
-console.log(myDate.toLocaleString())
+//let myDate = new Date()
+//console.log(myDate)
+//console.log(typeof(myDate))
+//console.log(myDate.toString())
+//console.log(myDate.toDateString())
+//console.log(myDate.toLocaleString())
 
 
-let mynewDate = new Date(2002, 9, 11)
-console.log(mynewDate.toString())
-console.log(mynewDate)
+//let mynewDate = new Date(2002, 9, 11)
+//console.log(mynewDate.toString())
+//console.log(mynewDate.toLocaleDateString())
+
+//let myTimestamp = Date.now()
+//console.log(myTimestamp) //milisecond output
+//console.log(mynewDate.getTime())
+//console.log(mynewDate)
+//console.log(Math.floor(Date.now()/1000))
+
+
+let oneDate = new Date(9, 11 , 2002)
+let twoDate = new Date(9, 12, 2002)
+
+//const oneTime = oneDate.getTime();
+//const twoTime = twoDate.getTime();
+
+let differenceDate = twoDate - oneDate
+console.log(differenceDate)
