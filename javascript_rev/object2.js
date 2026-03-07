5341156acd5177c432a5162a3bd89773ce34cdb3
Swarup143 <santiswarupnayak1@gmail.com>
2026-03-07 23:52:42 +0530

    object 2

---
+++ javascript_rev/object2.js
@@ -0,0 +1,20 @@
+const facebook = new Object()
+
+facebook.id = "san135"
+facebook.name = "San"
+facebook.isLoggedIn = false
+
+console.log(facebook)
+
+const regular = {
+
+    fullname: {
+        username: {
+            firstname: "Santiswarup",
+            lastname:"Nayak"
+        }
+    }
+
+}
+
+console.log(regular.fullname.username.firstname)
