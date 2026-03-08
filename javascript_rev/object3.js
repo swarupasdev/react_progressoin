4269e8557cd86cd1c03be985ffb22e7f9b339530
Swarup143 <santiswarupnayak1@gmail.com>
2026-03-08 23:22:28 +0530

    object 3rd part

---
+++ javascript_rev/object3.js
@@ -0,0 +1,15 @@
+const course = {
+
+    cName : 'js in hindi',
+    cPrice : '950',
+    cInstructor : 'swarup'
+}
+
+// course.cInstructor
+
+const { cInstructor: Instructure } = course   // you're changing a value inside an object 
+
+//console.log(cInstructor)
+console.log(Instructure)
+
+const navbar =()
