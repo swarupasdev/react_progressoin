17dbcdf58d84ccc6647cd9f5c0f63036188dce10
Swarup143 <santiswarupnayak1@gmail.com>
2026-03-12 22:47:43 +0530

    about swich and break

---
+++ javascript_rev/controlflow/file2.js
@@ -0,0 +1,22 @@
+//switch (key) {
+//    case value1:
+//        // Code to execute if expression === value1
+//        break;
+//    default:
+//    // Code to execute if none of the cases match
+//}
+const day = 3
+
+switch (day) {
+    case 1:
+        console.log("Sunday")
+        break;
+    case 2:
+        console.log("Monday")
+        break;
+    case 3:
+        console.log("Tuesday")
+        break;
+    default:
+        console.log("Not matched")
+}
