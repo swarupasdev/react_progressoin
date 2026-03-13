3e3c72cc3b797ad11227ac39f49d37cb5d0143e3
Swarup143 <santiswarupnayak1@gmail.com>
2026-03-13 11:27:30 +0530

    control flow finished

--- javascript_rev/controlflow/file3.js
+++ javascript_rev/controlflow/file3.js
@@ -8,3 +8,12 @@ if (age >= 18) {
 //falsy value
 //false, 0, -0, bigInt 0n,"",null,undefined,NaN 
 //true, {}, [], 42, "0", "false", -42, 3.14, -3.14, Infinity, -Infinity, Date(), function() {}, " ", Symbol(), 10n
+
+
+//nullish coalescing operator(??): null undefined
+
+let val1
+//val1 = 5 ?? 10
+val1 = null ?? 10
+var1=indefined??15
+console.log(val1)
