dd02bb54dafa24f633e39f2b7e8c0a37fb3afce7
Swarup143 <santiswarupnayak1@gmail.com>
2026-03-13 22:26:39 +0530

    for loop

---
+++ javascript_rev/iterations/forloop.js
@@ -0,0 +1,11 @@
+//for 
+//for (initialization; condition; afterthought) {
+  // code block to be executed  }
+
+//const myArray = [283, 274, 782, 823]
+for (let index = 0; index < 10; index++) {
+
+    const element = index
+    console.log(element)
+
+}
