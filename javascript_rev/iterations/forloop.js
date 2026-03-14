4c3dd408f67dc5b5f0b8ae49dac4dad6724f9847
Swarup143 <santiswarupnayak1@gmail.com>
2026-03-14 19:39:43 +0530

    again for loop

--- javascript_rev/iterations/forloop.js
+++ javascript_rev/iterations/forloop.js
@@ -5,7 +5,18 @@
 //const myArray = [283, 274, 782, 823]
 for (let index = 0; index < 10; index++) {
 
-    const element = index
-    console.log(element)
+    console.log(index,": Fuck Offfffff")
+
+};
+//console.log("Fuckkk Offffffffff")
+//
+for (let i = 9; i >= 0; i--) {
+
+    console.log(i, ": Fuck Offfffff")
+
+}
+for (let index = 0; index < 10; index+=2) {
+
+    console.log(index, ": Fuck Offfffff")
 
 }
