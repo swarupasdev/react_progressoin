0d58c8ad1ded1091c04d1b76bdbb529cba6a7706
Swarup143 <santiswarupnayak1@gmail.com>
2026-03-15 23:52:17 +0530

    fillermap

---
+++ javascript_rev/iterations/fillermap.js
@@ -0,0 +1,22 @@
+//const coding = ["js", "ruby", "java", "cpp"]
+//const value = coding.forEach(function (item) {
+//    console.log(item)
+//    return item
+//})
+//console.log(value)
+
+//filter
+const myNums = [1, 2, 3, 4, 5, 6]
+//const newNum= myNums.filter(function (num) {
+//return num>4
+//})
+//console.log(newNum)
+
+
+//map
+//const myNumbers = [1, 2, 3, 4, 5, 6]
+//const newNumbers = myNumbers.map(function (num1) {
+//    return num1+2
+//})
+//console.log(newNumbers)
+const myNumbers = [1, 2, 3, 4, 5, 6]
