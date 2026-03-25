c8fab748cef562537f4d8fbacd14f7ecb192fd61
Swarup143 <santiswarupnayak1@gmail.com>
2026-03-25 23:51:12 +0530

    finished with object

---
+++ javascript_rev/object/oop.js
@@ -0,0 +1,10 @@
+const user = {
+    name: "swarup",
+    loginCount: "8",
+    signedIn: true,
+    getUserDetails: function(){
+        console.log("got user details")
+        }
+}
+console.log(user.name)
+console.log(user.getUserDetails)
