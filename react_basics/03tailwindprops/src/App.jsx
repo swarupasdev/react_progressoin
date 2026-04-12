11d9004534e29a854b4ed5707f5be99fb1f83b68
Swarup143 <santiswarupnayak1@gmail.com>
2026-04-12 20:50:43 +0530

    tailwind and props and a react interview question

--- react_basics/03tailwindprops/src/App.jsx
+++ react_basics/03tailwindprops/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import './App.css'
+//import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-        <h1 className='' >Tailwind Test</h1>
+        <h1 className='bg-blue-900 p-4 text-red-500 rounded-xl'>Tailwind Test</h1>
     </>
   )
 }
