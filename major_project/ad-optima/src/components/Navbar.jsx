a430320ed1fe376e7bc73a12bbde8b94f9122d41
swarupasdev <santiswarupnayak1@gmail.com>
2026-04-23 22:35:54 +0530

    "Navigation bar created"

---
+++ major_project/ad-optima/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <nav style={{ display: "flex", justifyContent: "space-between" }}>
+      <h2>Ad-Optima</h2>
+
+      <div>
+        <button>Home</button>
+        <button>Login</button>
+        <button>Signup</button>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
