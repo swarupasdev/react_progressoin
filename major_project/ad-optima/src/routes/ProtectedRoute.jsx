61a7a5a044d161669aaa9f3b0420e434f2b0c9c2
swarupasdev <santiswarupnayak1@gmail.com>
2026-05-04 23:57:20 +0530

    protectedroute flow understanding done

--- major_project/ad-optima/src/routes/ProtectedRoute.jsx
+++ major_project/ad-optima/src/routes/ProtectedRoute.jsx
@@ -1,14 +1,17 @@
 import { useContext } from "react"
 import { Navigate } from "react-router-dom"
 import { AuthContext } from "../context/AuthContext"
-
-function ProtectedRoute({ children }) {
+//protected route for user without this anyone can access your dashboard and createcampaign 
+function ProtectedRoute({ children }) {  //AppRoute reference
   const { user, loading } = useContext(AuthContext)
+
+  //debugging line
   console.log("ProtectedRoute:", { user, loading, children })
 
-
+  //Without this App may redirect user before login restoration finishes.
   if (loading) return null
 
+  //replace removes current page from browser history
   if (!user) return <Navigate to="/login" replace />
 
   return children
